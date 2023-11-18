9f4fc91d09c469aeac83b0267db8dd02bbfce792
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-18 21:29:23 +0100

    Reversed string update

--- 01_reversedString.js
+++ 01_reversedString.js
@@ -1,3 +1,4 @@
+// I reverse()
 function reversedString(str) {
     
     let reversed = str.split("").reverse().join("");
@@ -5,3 +6,24 @@ function reversedString(str) {
 }
 
 console.log(reversedString("Just for a test"));
+
+// II for loop
+function reversedString(str) {
+
+    let reversed = "";
+    for(let i = str.length-1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+console.log(reversedString("Just for a test"));
+
+// III with recursion
+const reversed = (str) => {
+
+    if(str.length === 0) return "";
+    return reversed(str.slice(1)) + str[0];
+}
+
+console.log(reversed("Just for a test"))

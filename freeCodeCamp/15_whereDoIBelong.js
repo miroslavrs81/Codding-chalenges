75dfe0afa6fc3e7794118c7015d578782cdc0c81
miroslavrs81 <miroslav81ilic@gmail.com>
2023-12-09 17:55:02 +0100

    Where do I Belong

---
+++ freeCodeCamp/15_whereDoIBelong.js
@@ -0,0 +1,15 @@
+function getIndexToIns(arr, num) {
+
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+    
+    for(let i = 0; i < arr.length; i++) {
+      if(num <= arr[i]){
+        return i;
+      }
+    }
+    return arr.length;
+}
+
+console.log(getIndexToIns([ 40, 60, 5], 61));

a5a2c2376ab8320a4773a6978b6fbcaca029c788
miroslavrs81 <miroslav81ilic@gmail.com>
2023-12-13 21:13:33 +0100

    Returning Strings

---
+++ codeWars/10_returningStrings.js
@@ -0,0 +1,15 @@
+// I way
+
+function greet(name) {
+return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet('Miki'));
+
+
+// II way
+function greet(name) {
+    return "Hello, " + name + " how are you doing today?"
+}
+
+console.log(greet('Miki'));
